5cca9ab797280f103ce684aebe30044dfb6c2368
Rajan Phadnis <22627030+rajanphadnis@users.noreply.github.com>
2024-03-15 19:11:48 -0400

    added storage and basic pdf-lib tester

---
+++ public/main.js
@@ -0,0 +1,42 @@
+function readFileAsync(file) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader();
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsArrayBuffer(file);
+  });
+}
+function download(file, filename, type) {
+  const link = document.getElementById("link");
+  link.download = filename;
+  let binaryData = [];
+  binaryData.push(file);
+  link.href = URL.createObjectURL(new Blob(binaryData, { type: type }));
+}
+async function merge() {
+  let PDFDocument = PDFLib.PDFDocument;
+
+  const in1 = document.getElementById("file1").files[0];
+  const in2 = document.getElementById("file2").files[0];
+  let bytes1 = await readFileAsync(in1);
+  let bytes2 = await readFileAsync(in2);
+  const pdf1 = await PDFDocument.load(bytes1);
+  const pdf2 = await PDFDocument.load(bytes2);
+
+  const mergedPdf = await PDFDocument.create();
+  const copiedPagesA = await mergedPdf.copyPages(pdf1, pdf1.getPageIndices());
+  copiedPagesA.forEach((page) => mergedPdf.addPage(page));
+  const copiedPagesB = await mergedPdf.copyPages(pdf2, pdf2.getPageIndices());
+  copiedPagesB.forEach((page) => mergedPdf.addPage(page));
+  const mergedPdfFile = await mergedPdf.saveAsBase64({ dataUri: false });
+
+  showPDF(mergedPdfFile);
+}
+
+function showPDF(pdfDataUri) {
+  document.getElementById("iframe_preview").innerHTML = "<iframe src='" + pdfDataUri + "'></iframe>";
+//   document.getElementById("pdf-viewport").contentWindow.location.reload();
+  console.log("done");
+}
